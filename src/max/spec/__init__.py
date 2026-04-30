9bec9bd1b0b5f20877191ed450bbba9704beba02
TakaIshikawa <hiro52ishikawa.us@gmail.com>
2026-05-01 00:50:29 +0900

    Add spec ADR generator

--- src/max/spec/__init__.py
+++ src/max/spec/__init__.py
@@ -2,6 +2,10 @@
 
 from max.spec.experiment_card import generate_experiment_card
 from max.spec.acceptance_criteria import generate_acceptance_criteria
+from max.spec.adr import (
+    generate_architecture_decision_record,
+    render_architecture_decision_record_markdown,
+)
 from max.spec.bundle import generate_spec_bundle, render_spec_bundle_markdown, render_spec_bundle_yaml
 from max.spec.generator import generate_spec_preview
 from max.spec.implementation_plan import generate_implementation_plan
@@ -12,12 +16,14 @@ from max.spec.risk_register import generate_risk_register, render_risk_register_
 __all__ = [
     "evaluate_spec_readiness",
     "generate_acceptance_criteria",
+    "generate_architecture_decision_record",
     "generate_experiment_card",
     "generate_implementation_plan",
     "generate_launch_checklist",
     "generate_spec_bundle",
     "generate_risk_register",
     "render_risk_register_markdown",
+    "render_architecture_decision_record_markdown",
     "render_spec_bundle_yaml",
     "generate_spec_preview",
     "render_spec_bundle_markdown",
